4e52d4e46205e913efa805d69e90681ed7b722d9
Andrii <andre40000k@gmail.com>
2024-11-20 20:30:30 +0200

    Updated structure of the project

--- src/__test__/ageClassification.test.js
+++ src/__test__/ageClassification.test.js
@@ -1,20 +1,4 @@
-const ageClassification = (num) => {
-  return num < 0
-    ? null
-    : num <= 24
-    ? "Дитинство"
-    : num <= 44
-    ? "Молодість"
-    : num <= 65
-    ? "Зрілість"
-    : num <= 75
-    ? "Старість"
-    : num <= 90
-    ? "Довголіття"
-    : num <= 122
-    ? "Рекорд"
-    : null;
-};
+const { ageClassification } = require("../main");
 
 describe("Тестування ageClassification", () => {
   test("Повертає null для негативних чисел", () => {
--- src/__test__/weekFn.test.js
+++ src/__test__/weekFn.test.js
@@ -1,34 +1,4 @@
-const weekFn = (cond) => {
-  let str = "";
-
-  switch (cond) {
-    case 1:
-      str = "Понеділок";
-      break;
-    case 2:
-      str = "Вівторок";
-      break;
-    case 3:
-      str = "Середа";
-      break;
-    case 4:
-      str = "Четвер";
-      break;
-    case 5:
-      str = "П'ятниця";
-      break;
-    case 6:
-      str = "Субота";
-      break;
-    case 7:
-      str = "Неділя";
-      break;
-    default:
-      str = null;
-  }
-
-  return str;
-};
+const { weekFn } = require("../main");
 
 describe("Тестування weekFn", () => {
   test("Повертає правильний день тижня для чисел від 1 до 7", () => {
